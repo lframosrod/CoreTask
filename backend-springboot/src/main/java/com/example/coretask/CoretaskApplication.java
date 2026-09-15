0daae101820aebcec6de1db59b36d0270ef0bee9
lframosrod <lf.ramos.rod@gmail.com>
2026-09-14 20:45:22 -0700

    chore: inicializar proyecto

---
+++ backend-springboot/src/main/java/com/example/coretask/CoretaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.coretask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoretaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoretaskApplication.class, args);
+	}
+
+}
---
+++ backend-springboot/src/test/java/com/example/coretask/CoretaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coretask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoretaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
