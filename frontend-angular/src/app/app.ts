ba2006ef98f1b5cf074951a6f021e4ae20672624
lframosrod <lf.ramos.rod@gmail.com>
2026-09-16 13:17:35 -0700

    feat/ implementacion de angular material

--- frontend-angular/src/app/app.ts
+++ frontend-angular/src/app/app.ts
@@ -1,24 +1,28 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core'; // <-- Agregamos signal
 import { RouterOutlet } from '@angular/router';
 import { ApiService } from './api.service';
 import { JsonPipe } from '@angular/common'; 
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, JsonPipe],
-  templateUrl: './app.html', // <-- Apunta a tu archivo HTML
+  imports: [RouterOutlet, JsonPipe, MatCardModule, MatToolbarModule],
+  templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class AppComponent implements OnInit {
   private apiService = inject(ApiService);
-  backendData: any;
+  
+  backendData = signal<any>(null); 
 
   ngOnInit() {
     this.apiService.getTasksByUser(1).subscribe({
       next: (data) => {
-        console.log('Datos recibidos:', data);
-        this.backendData = data;
+        console.log('¡Datos interceptados por Angular!', data);
+        // Actualizamos el Signal, lo que fuerza a la pantalla a redibujarse
+        this.backendData.set(data); 
       },
       error: (err) => console.error('Error al conectar:', err)
     });
